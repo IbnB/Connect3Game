fff6858747cc4ade1a48c9973917944a25b920fe
IbnB <itahirou@outlook.com>
2016-07-07 18:08:25 +0200

    Game logic implemented, play again button reset the game board. but there is tie the method to reset the game has not been implemeneted yet.

--- app/src/main/java/com/example/ibrahimtahirou/connect3/MainActivity.java
+++ app/src/main/java/com/example/ibrahimtahirou/connect3/MainActivity.java
@@ -3,14 +3,25 @@ package com.example.ibrahimtahirou.connect3;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    /* 0= yellow, 1 = red */
+    // 0= yellow, 1 = red
     int activePlayer =0;
 
+    //2 means unplayed
     int[] gameState={2,2,2,2,2,2,2,2,2};
+
+    //winning positions : horizontal, vertical and diagonal
+    int[][] winningPositions={{0,1,2},{3,4,5},{6,7,8},
+                              {0,3,6},{1,4,7},{2,5,8},
+                              {0,4,8},{2,4,6} };
+
 
 
 
@@ -31,7 +42,52 @@ public class MainActivity extends AppCompatActivity {
                 counter.setImageResource(R.drawable.red);
                 activePlayer=0;
             }
+
+            for (int[] wingPosition : winningPositions){
+                if(gameState[wingPosition[0]]==gameState[wingPosition[1]] &&
+                        gameState[wingPosition[1]]==gameState[wingPosition[2]] &&
+                        gameState[wingPosition[0]]!=2){
+
+                    String winner  = "Red ";
+                    if(gameState[wingPosition[0]]==0){
+                        winner ="Yellow ";
+
+                    }
+
+                    TextView winnerMessage = (TextView)findViewById(R.id.winnerMessage);
+
+                    winnerMessage.setText( winner + "has won!");
+
+                    //after someone has won the game display the linear loyout wich asks user to play again
+                    LinearLayout layout =(LinearLayout)findViewById(R.id.playAgainLayout);
+                    layout.setVisibility(view.VISIBLE);
+
+                }
+            }
             counter.animate().translationYBy(1000f).setDuration(300);
+
+        }
+
+    }
+
+
+
+    public void playAgain(View view){
+
+        LinearLayout layout =(LinearLayout)findViewById(R.id.playAgainLayout);
+        layout.setVisibility(view.INVISIBLE);
+
+
+        for (int i=0; i<gameState.length; i++){
+            gameState[i]=2;
+
+        }
+
+        GridLayout gridLayout = (GridLayout)findViewById(R.id.gameBoardLayout);
+
+        for( int i =0; i< gridLayout.getChildCount(); i++){
+
+            ((ImageView) gridLayout.getChildAt(i)).setImageResource(0);
         }
 
     }
