7ab4d4f86db5e67c2237a4044eaaaafc568d8cfa
IbnB <itahirou@outlook.com>
2016-07-07 16:21:30 +0200

    Game Board fixed, once a player has placed his pad no other player can remove the pad from its original location.

--- app/src/main/java/com/example/ibrahimtahirou/connect3/MainActivity.java
+++ app/src/main/java/com/example/ibrahimtahirou/connect3/MainActivity.java
@@ -2,10 +2,39 @@ package com.example.ibrahimtahirou.connect3;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* 0= yellow, 1 = red */
+    int activePlayer =0;
 
+    int[] gameState={2,2,2,2,2,2,2,2,2};
+
+
+
+    public void dropIn(View view){
+        ImageView counter = (ImageView) view;
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter]==2){
+
+            gameState[tappedCounter]= activePlayer;
+
+            counter.setTranslationY(-1000f);
+            if (activePlayer==0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer=1;
+            }
+            else{
+                counter.setImageResource(R.drawable.red);
+                activePlayer=0;
+            }
+            counter.animate().translationYBy(1000f).setDuration(300);
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
